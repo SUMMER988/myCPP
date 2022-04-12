7e8a64bd9fceff32d64816b4f9ff5d838e7c2d69
summerTime329 <10608509+summertime329@user.noreply.gitee.com>
2022-04-13 02:04:32 +0800

    LNode

--- 2.3.2_LNode.cpp
+++ 2.3.2_LNode.cpp
@@ -7,7 +7,7 @@ typedef int ElemType;
 typedef struct LNode
 {
     ElemType data;
-    int length;
+    struct LNode *head, *tail;
     struct LNode *next;
 } LNode, *LinkList;
 bool InitList(LinkList &L)
@@ -17,8 +17,72 @@ bool InitList(LinkList &L)
     {
         return false;
     }
+    L->head = L;
+    L->tail = L;
     L->next = nullptr;
-    L->length = 0;
+    return true;
+}
+// 头插法
+bool HeadInsert(LinkList &L, ElemType e)
+{
+    if (!L)
+    {
+        return false;
+    }
+    LNode *p = new LNode;
+    p->data = e;
+    p->next = L->next;
+    L->next = p;
+    return true;
+}
+// 尾插法
+bool TailInsert(LinkList &L, ElemType e)
+{
+    if (!L)
+    {
+        return false;
+    }
+    LNode *p = new LNode;
+    p->data = e;
+    p->next = L->tail->next;
+    L->tail->next = p;
+    L->tail = p;
+    return true;
+}
+// 指定结点的后插操作
+bool InsertNextNode(LNode *p, ElemType e)
+{
+    if (!p)
+    {
+        return false;
+    }
+    LNode *s = new LNode;
+    if (!s)
+    {
+        return false;
+    }
+    s->data = e;
+    s->next = p->next;
+    p->next = s;
+
+    return true;
+}
+// 指定结点的前插操作
+bool InsertPriorNode(LNode *p, ElemType e)
+{
+    if (!p)
+    {
+        return false;
+    }
+    LNode *s = new LNode;
+    if (!s)
+    {
+        return false;
+    }
+    s->next = p->next;
+    p->next = s;
+    s->data = p->data;
+    p->data = e;
     return true;
 }
 // 按位序插入——带头结点
@@ -37,23 +101,55 @@ bool ListInsert(LinkList &L, int pos, ElemType e)
     }
     return InsertNextNode(p, e);
 }
-//指定结点的后插操作
-bool InsertNextNode(LNode *p, ElemType e)
+// 按位序删除——带头结点
+bool ListDelete(LinkList &L, int pos, ElemType &e)
+{
+    if (pos < 1)
+    {
+        return false;
+    }
+    int target = 0;
+    LNode *p = L;
+    while (!p && target < pos - 1)
+    {
+        p = p->next;
+        target++;
+    }
+    if (!p || !p->next)
+    {
+        return false;
+    }
+    LNode *q = p->next;
+    p->next = q->next;
+    e = q->data;
+    free(q);
+    return true;
+}
+// 删除指定结点
+bool DeleteNode(LNode *p)
 {
     if (!p)
     {
         return false;
     }
-    LNode *s = new LNode;
-    if (!s)
-    {
-        return false;
-    }
-    s->data = e;
-    s->next = p->next;
-    p->next = s;
+    LNode *q = p->next;
+    p->data = q->data;
+    p->next = q->next;
+    delete q;
     return true;
 }
+// 删除全部
+void ListDeleteAll(LinkList &L)
+{
+    if (!L->next)
+    {
+        return;
+    }
+    ListDeleteAll(L->next);
+    delete L->next;
+    L->next = nullptr;
+}
+// 打印单链表元素
 void PrintList(LinkList L)
 {
     LNode *head = L->next;
@@ -70,7 +166,19 @@ void PrintList(LinkList L)
         head = head->next;
     }
 }
+// 判断表空
+
+bool EmptyList(LinkList L)
+{
+    return (!L->next);
+}
 int main()
 {
-    LinkList L = nullptr;
+    LinkList L;
+    InitList(L);
+    TailInsert(L, 1);
+    TailInsert(L, 2);
+    TailInsert(L, 3);
+    TailInsert(L, 4);
+    PrintList(L);
 }
